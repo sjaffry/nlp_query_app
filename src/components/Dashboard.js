29ebcc85550284acfbd92cac7a4f29d84cb52a7b
syed jaffry <sajaffry@gmail.com>
2023-12-29 21:22:27 -0800

    removed LLM Query

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -46,30 +46,6 @@ const Dashboard = ({
           />
         </Paper>
       </Box>
-      <Box sx={{ display: 'flex', mb: 6, justifyContent: 'space-between' }}>
-        <Paper sx={{ width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', p: 2, borderColor: 'black', border: 0.3 }}>
-          <Typography variant="h5" gutterBottom>Q&A</Typography>
-          <TextField 
-            multiline 
-            variant="outlined" 
-            rows={3} 
-            sx={{ mb: 2 }} 
-            value={query} 
-            onChange={(e) => setQuery(e.target.value)} 
-            placeholder="Write your question here.." />
-          <TextField 
-            multiline 
-            variant="outlined" 
-            rows={7} 
-            value={response ? response : ''} 
-            placeholder="Answer.." />
-          <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-            <Button variant="outlined" sx={{ mr: 1, borderColor: 'black' }} onClick={handleSubmit}>Submit</Button>
-            <Button variant="outlined" sx={{ borderColor: 'black' }} onClick={() => { setQuery(''); setResponse(null); }}>Clear</Button>
-          </Box>
-          {submitLoading && <CircularProgress />}
-        </Paper>
-      </Box>
     </Box>
   );
 };
