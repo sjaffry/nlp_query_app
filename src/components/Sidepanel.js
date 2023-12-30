eb5800202603a3cce833dd7e3be13e4f399313df
syed jaffry <sajaffry@gmail.com>
2023-12-29 22:07:42 -0800

    minor cosmetic change

--- src/components/Sidepanel.js
+++ src/components/Sidepanel.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import { Box, Button, Typography, Paper, List, ListItem, ListItemIcon, TextField, CircularProgress } from '@mui/material';
+import { Box, Typography, Paper, List, ListItem, ListItemIcon } from '@mui/material';
 import { Link } from 'react-router-dom';
-import HomeIcon from '@mui/icons-material/Home';
-import BarChartIcon from '@mui/icons-material/BarChart';
+import LocalActivityIcon from '@mui/icons-material/LocalActivity';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import EventRepeatIcon from '@mui/icons-material/EventRepeat';
 
 const Sidepanel = ({
     business_name
@@ -13,19 +13,19 @@ const Sidepanel = ({
         <Typography variant="h4" gutterBottom color='#6366F1'>{business_name}</Typography>
         <List>
             <ListItem sx={{ mb: 2 }}>
-            <ListItemIcon><HomeIcon sx={{ color: 'white' }}/></ListItemIcon>
+            <ListItemIcon><EventRepeatIcon sx={{ color: 'white' }}/></ListItemIcon>
             <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
                 Periodic
             </Link>
             </ListItem>
             <ListItem sx={{ mb: 2 }}>
-            <ListItemIcon><BarChartIcon sx={{ color: 'white' }}/></ListItemIcon>
+            <ListItemIcon><LocalActivityIcon sx={{ color: 'white' }}/></ListItemIcon>
             <Link to="/Events_summary" style={{ color: 'white', textDecoration: 'none' }}>
                 Events
             </Link>
             </ListItem>
             <ListItem sx={{ mb: 2 }}>
-            <ListItemIcon><BarChartIcon sx={{ color: 'white' }}/></ListItemIcon>
+            <ListItemIcon><AssignmentIcon sx={{ color: 'white' }}/></ListItemIcon>
             <Link to="/Ad_hoc_summary" style={{ color: 'white', textDecoration: 'none' }}>
                 Ad-hoc
             </Link>
