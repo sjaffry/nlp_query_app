f744f633b2e560b2c1ce5e231e3d11af457c5854
syed jaffry <sajaffry@gmail.com>
2024-01-03 17:19:10 -0800

    added spinner to DownloadReviews

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { Box, Button, Typography, Paper, List, ListItem, ListItemIcon, TextField, CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
-import handleDownloadClick from './ReviewDownload';
 
 const Dashboard = ({
   summaryLoading,
@@ -12,6 +11,59 @@ const Dashboard = ({
   jwtToken,
   eventName
 }) => {
+
+  const [isDownloading, setIsDownloading] = useState(false);
+
+  const handleDownloadClick = async (jwtToken, reviewDate, eventName) => {
+    
+    setIsDownloading(true);
+    var file_prefix = 'transcribe-output/FTSC/';
+    var reviewsFileName = 'original-reviews-';
+
+    if (eventName == '') {
+       file_prefix += reviewDate+'/combinedreviews.csv';
+       reviewsFileName += reviewDate+'.csv'
+     } else {
+       file_prefix += 'events/'+eventName+'/'+reviewDate+'/combinedreviews.csv';
+       reviewsFileName += eventName+'-'+reviewDate+'.csv'
+     }
+
+    fetch('https://ctxzkwvz2a.execute-api.us-east-1.amazonaws.com/Prod/?file_prefix='+file_prefix, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': jwtToken
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.blob();
+        })
+        .then((blob) => {
+          // Create a temporary URL for the blob
+          const url = window.URL.createObjectURL(blob);
+  
+          // Create a hidden anchor element to trigger the download
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = url;
+          a.download = reviewsFileName;
+          document.body.appendChild(a);
+  
+          // Trigger the click event on the anchor element to start the download
+          a.click();
+  
+          // Clean up the temporary URL and anchor element
+          window.URL.revokeObjectURL(url);
+          document.body.removeChild(a);
+          setIsDownloading(false);
+        })
+        .catch((error) => {
+          console.error('Error downloading file:', error);
+        });
+  } 
   return (
     <Box>    
       <Box sx={{ display: 'flex', mb: 6, justifyContent: 'space-between' }}>
@@ -47,7 +99,7 @@ const Dashboard = ({
       <Box sx={{ display: 'flex', mb: 6 }}>
         <Button 
           variant="contained" 
-          disabled={summary == null || jwtToken == ''}
+          disabled={summary == null || jwtToken == '' || isDownloading}
           onClick= {() => handleDownloadClick(jwtToken, reviewDate, eventName)}
           sx={
             {width: '30%', p: 2, 
@@ -56,6 +108,7 @@ const Dashboard = ({
             }}> 
             Download original responses
         </Button>
+        {isDownloading && <CircularProgress />}
       </Box>
     </Box>
   );
