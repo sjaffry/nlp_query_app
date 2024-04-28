7dd4df591c3adcb6063fb7c730142badc854f5de
syed jaffry <sajaffry@gmail.com>
2024-04-28 10:22:26 -0700

    minor optimization

--- lambda_backend/nlp-query-pageload/nlpQueryPageLoad.py
+++ lambda_backend/nlp-query-pageload/nlpQueryPageLoad.py
@@ -3,6 +3,9 @@ import json
 import os
 import boto3
 
+s3 = boto3.client('s3')
+bucket_name = os.environ['bucket_name']
+
 def decode_base64_url(data):
     """Add padding to the input and decode base64 url"""
     missing_padding = len(data) % 4
@@ -11,9 +14,9 @@ def decode_base64_url(data):
     return base64.urlsafe_b64decode(data)
 
 def decode_jwt(token):
-    """Split the token and decode each part"""
+    """Decode JWT token and return payload"""
     parts = token.split('.')
-    if len(parts) != 3:  # a valid JWT has 3 parts
+    if len(parts) != 3:
         raise ValueError('Token is not valid')
 
     header = decode_base64_url(parts[0])
@@ -21,14 +24,11 @@ def decode_jwt(token):
     signature = decode_base64_url(parts[2])
 
     return json.loads(payload)
-    #return { 'business_name': payload['cognito:groups'][0] }
-    
+
 def trimmed_foldername(full_folderpath):
     return os.path.basename(os.path.normpath(full_folderpath))
-    
 
-def list_subfolders(bucket_name, prefix):
-    s3 = boto3.client('s3')
+def list_subfolders(prefix):
     response = s3.list_objects_v2(Bucket=bucket_name, Delimiter='/', Prefix=prefix)
     subfolders = []
 
@@ -38,39 +38,33 @@ def list_subfolders(bucket_name, prefix):
             subfolders.append(folder_name)
     return subfolders
 
-
 def lambda_handler(event, context):
-    bucket_name = os.environ['bucket_name']
     get_events = event["queryStringParameters"]['get_events']
     keep_warm = event["queryStringParameters"]['keep_warm']
-    
-    # Let's extract the business name from the token by looking at the group memebership of the user
     token = event['headers']['Authorization']
     decoded = decode_jwt(token)
-    # We only ever expect the user to be in one group only - business rule
     business_name = decoded['cognito:groups'][0]
-    
+
     if keep_warm == "true":
         return {'body': json.dumps('stay warm!')}
-    
-    # Now we list all the subfolders for the business name
+
     if get_events == "True":
         prefix = f'transcribe-output/{business_name}/events/'
     else:
         prefix = f'transcribe-output/{business_name}/'
-    
-    subfolders = list_subfolders(bucket_name, prefix)
+
+    subfolders = list_subfolders(prefix)
     result = {
         "Business name": business_name,
         "Subfolders": subfolders
-        }
-        
+    }
+
     return {
         'statusCode': 200,
         'headers': {
             "Access-Control-Allow-Headers" : "Content-Type",
             "Access-Control-Allow-Origin": "https://query.onreaction.com",
             "Access-Control-Allow-Methods": "OPTIONS,PUT,POST,GET"
-    },    
+        },
         'body': json.dumps(result)
-    } 
+    }
